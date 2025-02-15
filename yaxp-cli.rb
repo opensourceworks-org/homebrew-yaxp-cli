3e6e013b0f9e6ecd11bd9bdf23b0022a081f7d41
jeroenflvr <jeroen@flexworks.eu>
2025-02-15 18:00:52 +0100

    ci: correct asset sig

--- yaxp-cli.rb
+++ yaxp-cli.rb
@@ -2,7 +2,7 @@ class YaxpCli < Formula
     desc "<yaxp ⚡> Yet Another XSD Parser"
     homepage "https://github.com/opensourceworks-org/homebrew-yaxp-cli"
     url "https://github.com/opensourceworks-org/homebrew-yaxp-cli/releases/download/v0.2.2/macos-arm64-v0.2.2.tar.gz"
-    sha256 "5d1e8bdd55af522ab5a1eff9e1783f7d1df347c096fba18d7de22b66f18d7161"
+    sha256 "5b16bcff134ec8234d4c873e9005520c5cb065e4b3a421277273be157b87e6d1"
     license "MIT"
   
     depends_on "rust" => :build
