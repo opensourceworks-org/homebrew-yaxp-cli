9bcb61a49fb3e857c6dbf80bb0ec9ac2fc3d2fb7
jeroenflvr <jeroen@flexworks.eu>
2025-02-15 18:26:38 +0100

    chore: update spacing for rb

--- yaxp-cli.rb
+++ yaxp-cli.rb
@@ -1,20 +1,20 @@
 class YaxpCli < Formula
-    desc "<yaxp ⚡> Yet Another XSD Parser"
-    homepage "https://github.com/opensourceworks-org/homebrew-yaxp-cli"
-    url "https://github.com/opensourceworks-org/homebrew-yaxp-cli/releases/download/v0.2.2/macos-arm64-v0.2.2.tar.gz"
-    sha256 "5b16bcff134ec8234d4c873e9005520c5cb065e4b3a421277273be157b87e6d1"
-    license "MIT"
+  desc "<yaxp ⚡> Yet Another XSD Parser"
+  homepage "https://github.com/opensourceworks-org/homebrew-yaxp-cli"
+  url "https://github.com/opensourceworks-org/homebrew-yaxp-cli/releases/download/v0.2.2/macos-arm64-v0.2.2.tar.gz"
+  sha256 "5b16bcff134ec8234d4c873e9005520c5cb065e4b3a421277273be157b87e6d1"
+  license "MIT"
   
-    depends_on "rust" => :build
+  depends_on "rust" => :build
   
-    def install
-        # Install the pre-built binary into Homebrew's bin directory.
-        bin.install "yaxp-cli"
-    end
+  def install
+    # Install the pre-built binary into Homebrew's bin directory.
+    bin.install "yaxp-cli"
+  end
   
-    test do
-      # A basic test to verify that the binary was installed correctly.
-      assert_match "yaxp-cli 0.2.2", shell_output("#{bin}/yaxp-cli --version")
-    end
+  test do
+    # A basic test to verify that the binary was installed correctly.
+    assert_match "yaxp-cli 0.2.2", shell_output("#{bin}/yaxp-cli --version")
+  end
   end
   
