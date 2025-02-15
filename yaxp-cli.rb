f8f4486909cb592ff91e4a803bcaccf82ac1cb18
jeroenflvr <jeroen@flexworks.eu>
2025-02-15 17:40:03 +0100

    chore: update url

--- yaxp-cli.rb
+++ yaxp-cli.rb
@@ -1,6 +1,6 @@
 class MyRustCli < Formula
     desc "<yaxp ⚡> Yet Another XSD Parser"
-    homepage "https://github.com/opensourceworks-org/yaxp"
+    homepage "https://github.com/opensourceworks-org/homebrew-yaxp-cli"
     url "https://github.com/opensourceworks-org/homebrew-yaxp-cli/releases/download/v0.2.2/macos-arm64-v0.2.2.tar.gz"
     sha256 "5d1e8bdd55af522ab5a1eff9e1783f7d1df347c096fba18d7de22b66f18d7161"
     license "MIT"
